cb5f71f107dbac23d57d5f19b4455f99de204823
Matias Herrera <matiassh20@gmail.com>
2021-01-21 22:00:50 -0300

    sacando variables no utilizadas

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ class Solution{
     
     static solution(A){
 
-        let numbersWithCount = {};
-        let numbersCounting = {}
         let numberSlice = {};
         let maxSlice = 0;
         let numberWithMaxSlice;
 
         for (let number of A) {
-           if(numbersCounting[number] == undefined){
+           if(numberSlice[number] == undefined){
                 let {max, min} = Solution.getMaxAndMinPosition(A, number);
                 numberSlice[number] = (max+1) - min;
-                numbersCounting[number] = true;
 
                 if(numberSlice[number] > maxSlice){
                     maxSlice = numberSlice[number];
@@ -44,5 +41,5 @@ class Solution{
 }
 
 console.log(Solution.solution([
-    4,1,5,6,5,6,6,5
+    4,1,5,6,5,6,6,5,6,6,4
 ]));
